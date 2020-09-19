35d0605a7d858fbbc2582700b377e8e4d2188e6f
Jenya <jeenjenya@gmail.com>
2020-09-19 20:54:04 +0300

    test task for Emphasoft

---
+++ script.js
@@ -0,0 +1,14 @@
+
+ScrollReveal().reveal('.wrapper',{ 
+  distance: '200px', 
+  opacity: 0 ,
+  easing: 'ease-in',
+  delay: 350 })
+
+
+  function btnclck(){
+    if (window.confirm('Жми ОК и глянешь исходники на гитхабе :)')) 
+    {
+    window.location.href='https://github.com/ojenya/test-emphasoft/';
+    };
+  }
